de25ff210d3f8a1c9f0c8b9af56f54fe7182be4a
claragiavelli <32523485+claragiavelli@users.noreply.github.com>
2017-11-10 22:55:31 +0100

    Add files via upload

---
+++ produit/testprixunite.php
@@ -0,0 +1,12 @@
+<?php
+	ini_set('display_errors','1');
+	require_once("produitunite.php");
+	$pk = new ProduitUnite();
+	echo $pu;
+	$pk->poids = 2 ;
+	$pk->prixunit = 1;
+	echo $pu;
+	$prixu = $c->prixunit();	
+	echo "<br><br> Le prix est : ".$prixunit;
+
+?>
